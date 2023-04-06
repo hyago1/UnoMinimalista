8bb20d659afad95059b7b135c1ee4c6de4f39032
Hyago Philip <36174848+hyago1@users.noreply.github.com>
2023-04-06 15:55:43 -0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -7,8 +8,8 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/game.html');
-  });
+  res.sendFile(__dirname + '/game.html');
+});
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
@@ -17,273 +18,274 @@ var p = 10;
 
 
 const cards = [
-    {
-      cod: "B+4",
-      color: "#0f0f0f",
-      number: "M4",
-    },
-    {
-      cod: "B+4",
-      color: "#0f0f0f",
-      number: "M4",
-    },
-    {
-      cod: "B+4",
-      color: "#0f0f0f",
-      number: "M4",
-    },
-    {
-      cod: "B+4",
-      color: "#0f0f0f",
-      number: "M4",
-    },
-    {
-      cod: "R+2",
-      color: "#d60b11",
-      number: "M2",
-    },
-    {
-      cod: "R1",
-      color: "#d60b11",
-      number: 1,
-    },
-    {
-      cod: "BlockR",
-      color: "#d60b11",
-      number: "@",
-    },
-    {
-      cod: "R2",
-      color: "#d60b11",
-      number: 2,
-    },
-    {
-      cod: "R3",
-      color: "#d60b11",
-      number: 3,
-    },
-    {
-      cod: "R4",
-      color: "#d60b11",
-      number: 4,
-    },
-    {
-      cod: "R5",
-      color: "#d60b11",
-      number: 5,
-    },
-    {
-      cod: "R6",
-      color: "#d60b11",
-      number: 6,
-    },
-    {
-      cod: "R7",
-      color: "#d60b11",
-      number: 7,
-    },
-    {
-      cod: "R8",
-      color: "#d60b11",
-      number: 8,
-    },
-    {
-      cod: "R9",
-      color: "#d60b11",
-      number: 9,
-    },
-    {
-      cod: "G1",
-      color: "#67bd42",
-      number: 1,
-    },
-    {
-      cod: "BlockG",
-      color: "#67bd42",
-      number: "@",
-    },
-    {
-      cod: "G+2",
-      color: "#67bd42",
-      number: "M2",
-    },
-    {
-      cod: "G2",
-      color: "#67bd42",
-      number: 2,
-    },
-    {
-      cod: "G3",
-      color: "#67bd42",
-      number: 3,
-    },
-    {
-      cod: "G4",
-      color: "#67bd42",
-      number: 4,
-    },
-    {
-      cod: "G5",
-      color: "#67bd42",
-      number: 5,
-    },
-    {
-      cod: "G6",
-      color: "#67bd42",
-      number: 6,
-    },
-    {
-      cod: "G7",
-      color: "#67bd42",
-      number: 7,
-    },
-    {
-      cod: "G8",
-      color: "#67bd42",
-      number: 8,
-    },
-    {
-      cod: "G9",
-      color: "#67bd42",
-      number: 9,
-    },
-    {
-      cod: "B1",
-      color: "#133fc2",
-      number: 1,
-    },
-    {
-      cod: "BlockB",
-      color: "#133fc2",
-      number: "@",
-    },
-    {
-      cod: "B+2",
-      color: "#133fc2",
-      number: "M2",
-    },
-    {
-      cod: "B2",
-      color: "#133fc2",
-      number: 2,
-    },
-    {
-      cod: "B3",
-      color: "#133fc2",
-      number: 3,
-    },
-    {
-      cod: "B4",
-      color: "#133fc2",
-      number: 4,
-    },
-    {
-      cod: "B5",
-      color: "#133fc2",
-      number: 5,
-    },
-    {
-      cod: "B6",
-      color: "#133fc2",
-      number: 6,
-    },
-    {
-      cod: "B7",
-      color: "#133fc2",
-      number: 7,
-    },
-    {
-      cod: "B8",
-      color: "#133fc2",
-      number: 8,
-    },
-    {
-      cod: "B9",
-      color: "#133fc2",
-      number: 9,
-    },
-    {
-      cod: "Y1",
-      color: "#f7f01e",
-      number: 1,
-    },
-    {
-      cod: "BlockY",
-      color: "#f7f01e",
-      number: "@",
-    },
-    {
-      cod: "Y+2",
-      color: "#f7f01e",
-      number: "M2",
-    },
-    {
-      cod: "Y1",
-      color: "#f7f01e",
-      number: 2,
-    },
-    {
-      cod: "Y3",
-      color: "#f7f01e",
-      number: 3,
-    },
-    {
-      cod: "Y4",
-      color: "#f7f01e",
-      number: 4,
-    },
-    {
-      cod: "Y5",
-      color: "#f7f01e",
-      number: 5,
-    },
-    {
-      cod: "Y6",
-      color: "#f7f01e",
-      number: 6,
-    },
-    {
-      cod: "Y7",
-      color: "#f7f01e",
-      number: 7,
-    },
-    {
-      cod: "Y8",
-      color: "#f7f01e",
-      number: 8,
-    },
-    {
-      cod: "Y9",
-      color: "#f7f01e",
-      number: 9,
-    },
-    {
-      cod: "Joker",
-      color: "#0f0f0f",
-      number: "()",
-    },
-    {
-      cod: "Joker",
-      color: "#0f0f0f",
-      number: "()",
-    },
-    {
-      cod: "Joker",
-      color: "#0f0f0f",
-      number: "()",
-    },
-    {
-      cod: "Joker",
-      color: "#0f0f0f",
-      number: "()",
-    },
-  ];
+  {
+    cod: "B+4",
+    color: "#0f0f0f",
+    number: "M4",
+  },
+  {
+    cod: "B+4",
+    color: "#0f0f0f",
+    number: "M4",
+  },
+  {
+    cod: "B+4",
+    color: "#0f0f0f",
+    number: "M4",
+  },
+  {
+    cod: "B+4",
+    color: "#0f0f0f",
+    number: "M4",
+  },
+  {
+    cod: "R+2",
+    color: "#d60b11",
+    number: "M2",
+  },
+  {
+    cod: "R1",
+    color: "#d60b11",
+    number: 1,
+  },
+  {
+    cod: "BlockR",
+    color: "#d60b11",
+    number: "@",
+  },
+  {
+    cod: "R2",
+    color: "#d60b11",
+    number: 2,
+  },
+  {
+    cod: "R3",
+    color: "#d60b11",
+    number: 3,
+  },
+  {
+    cod: "R4",
+    color: "#d60b11",
+    number: 4,
+  },
+  {
+    cod: "R5",
+    color: "#d60b11",
+    number: 5,
+  },
+  {
+    cod: "R6",
+    color: "#d60b11",
+    number: 6,
+  },
+  {
+    cod: "R7",
+    color: "#d60b11",
+    number: 7,
+  },
+  {
+    cod: "R8",
+    color: "#d60b11",
+    number: 8,
+  },
+  {
+    cod: "R9",
+    color: "#d60b11",
+    number: 9,
+  },
+  {
+    cod: "G1",
+    color: "#67bd42",
+    number: 1,
+  },
+  {
+    cod: "BlockG",
+    color: "#67bd42",
+    number: "@",
+  },
+  {
+    cod: "G+2",
+    color: "#67bd42",
+    number: "M2",
+  },
+  {
+    cod: "G2",
+    color: "#67bd42",
+    number: 2,
+  },
+  {
+    cod: "G3",
+    color: "#67bd42",
+    number: 3,
+  },
+  {
+    cod: "G4",
+    color: "#67bd42",
+    number: 4,
+  },
+  {
+    cod: "G5",
+    color: "#67bd42",
+    number: 5,
+  },
+  {
+    cod: "G6",
+    color: "#67bd42",
+    number: 6,
+  },
+  {
+    cod: "G7",
+    color: "#67bd42",
+    number: 7,
+  },
+  {
+    cod: "G8",
+    color: "#67bd42",
+    number: 8,
+  },
+  {
+    cod: "G9",
+    color: "#67bd42",
+    number: 9,
+  },
+  {
+    cod: "B1",
+    color: "#133fc2",
+    number: 1,
+  },
+  {
+    cod: "BlockB",
+    color: "#133fc2",
+    number: "@",
+  },
+  {
+    cod: "B+2",
+    color: "#133fc2",
+    number: "M2",
+  },
+  {
+    cod: "B2",
+    color: "#133fc2",
+    number: 2,
+  },
+  {
+    cod: "B3",
+    color: "#133fc2",
+    number: 3,
+  },
+  {
+    cod: "B4",
+    color: "#133fc2",
+    number: 4,
+  },
+  {
+    cod: "B5",
+    color: "#133fc2",
+    number: 5,
+  },
+  {
+    cod: "B6",
+    color: "#133fc2",
+    number: 6,
+  },
+  {
+    cod: "B7",
+    color: "#133fc2",
+    number: 7,
+  },
+  {
+    cod: "B8",
+    color: "#133fc2",
+    number: 8,
+  },
+  {
+    cod: "B9",
+    color: "#133fc2",
+    number: 9,
+  },
+  {
+    cod: "Y1",
+    color: "#f7f01e",
+    number: 1,
+  },
+  {
+    cod: "BlockY",
+    color: "#f7f01e",
+    number: "@",
+  },
+  {
+    cod: "Y+2",
+    color: "#f7f01e",
+    number: "M2",
+  },
+  {
+    cod: "Y1",
+    color: "#f7f01e",
+    number: 2,
+  },
+  {
+    cod: "Y3",
+    color: "#f7f01e",
+    number: 3,
+  },
+  {
+    cod: "Y4",
+    color: "#f7f01e",
+    number: 4,
+  },
+  {
+    cod: "Y5",
+    color: "#f7f01e",
+    number: 5,
+  },
+  {
+    cod: "Y6",
+    color: "#f7f01e",
+    number: 6,
+  },
+  {
+    cod: "Y7",
+    color: "#f7f01e",
+    number: 7,
+  },
+  {
+    cod: "Y8",
+    color: "#f7f01e",
+    number: 8,
+  },
+  {
+    cod: "Y9",
+    color: "#f7f01e",
+    number: 9,
+  },
+  {
+    cod: "Joker",
+    color: "#0f0f0f",
+    number: "()",
+  },
+  {
+    cod: "Joker",
+    color: "#0f0f0f",
+    number: "()",
+  },
+  {
+    cod: "Joker",
+    color: "#0f0f0f",
+    number: "()",
+  },
+  {
+    cod: "Joker",
+    color: "#0f0f0f",
+    number: "()",
+  },
+];
 const jokerCard = { cod: "Joker", number: "X" }
 
 var scramble = new Array();
 scramble.length = 7;
 var scramble2 = new Array();
 scramble2.length = 7;
+var pileStart;
 var pile;
 var game = new Array();
 game.length = 1000;
@@ -300,56 +302,218 @@ var turn = 1;
 
 
 
+var n = Math.floor(Math.random() * cards.length);
+for (let i = 0; i < game.length; i++) {
+  if (game[i] == null) {
+    game[i] = n;
+  }
 
-
-
-
-     var n = Math.floor(Math.random() * cards.length);
-        for (let i = 0; i < game.length; i++) {
-          if (game[i] == null) {
-            game[i] = n;
-          }
-      
-          lastColor = cards[n].color;
-          lastNumber = cards[n].number;
-          console.log(lastColor + " Começo");
-          console.log(lastNumber + " Numero");
-          pile = `
+  lastColor = cards[n].color;
+  lastNumber = cards[n].number;
+  console.log(lastColor + " Começo");
+  console.log(lastNumber + " Numero");
+  pileStart = `
           <div  style="background-color:${cards[n].color};" class="card" id="${i}" > 
           <div class="cornerUpDiv">
-          <span id="cornerUp"> 1</span> </div> <span> 1</span> 
+          <span id="cornerUp"> ${specialCard(
+            cards[n].number,
+            "u"
+          )}</span> </div> <span> ${specialCard(
+            cards[n].number,
+            "m"
+          )}</span> 
           <div class="cornerDownDiv">
-          <span id="cornerDown"> 1</span></div> 
+          <span id="cornerDown"> ${specialCard(
+            cards[n].number,
+            "d"
+          )}</span></div> 
           </div>`;
-      
-          break;
-        }
 
-            for (let i = 0; i < scramble2.length; i++) {
-              if (scramble[i] == null) {
-                scramble[i] = Math.floor(Math.random() * cards.length);
-              }
-            }
-            for (let i = 0; i < scramble2.length; i++) {
-              if (scramble2[i] == null) {
-                scramble2[i] = Math.floor(Math.random() * cards.length);
-              }
-            }
+
           
-         
+  break;
+}
 
+for (let i = 0; i < scramble2.length; i++) {
+  if (scramble[i] == null) {
+    scramble[i] = Math.floor(Math.random() * cards.length);
+  }
+}
+for (let i = 0; i < scramble2.length; i++) {
+  if (scramble2[i] == null) {
+    scramble2[i] = Math.floor(Math.random() * cards.length);
+  }
+}
+
+
+
+
+function specialCard(typeCard, U, player) {
+  var circleMainBlock = `<div class="circleBlock"><div class="diagonal"></div></div>`;
+  var circleUBlock = `<div class="circleUDBlock"><div class="diagonal"></div></div>`;
+  var circleDBlock = `<div class="circleUDBlock"><div class="diagonal"></div></div>`;
+  var circleMain = `<div class="circle"></div>`;
+  var circleU = `<div class="circleUD"></div>`;
+  var circleD = `<div class="circleUD"></div>`;
+
+  if (typeCard == "()") {
+    if (U == "u") {
+      return circleU;
+    }
+    if (U == "d") {
+      return circleD;
+    }
+    if (U == "m") {
+      return circleMain;
+    }
+  }
+
+  if (typeCard == "@") {
+    if (player == 1) {
+      turn = 1
+    }
+    if (player == 2) {
+      turn = 2
+    }
+    if (U == "u") {
+      return circleUBlock;
+    }
+    if (U == "d") {
+      return circleDBlock;
+    }
+    if (U == "m") {
+      return circleMainBlock;
+    }
+  }
+
+  return typeCard;
+}
+
+
+
+
+
+function verificationCard(scram) {
+  console.log(cards[scram].color);
+  console.log(cards[scram].cod);
+
+  if (cards[scram].color == lastColor) {
+    console.log("True color");
+    validation = true;
+  } else if (cards[scram].number == lastNumber) {
+    validation = true;
+    console.log("True number");
+  } else if (cards[scram].color == "#0f0f0f") {
+    validation = true;
+  }
+
+  return validation;
+}
 
 
 io.on('connection', (socket) => {
-   
 
-    console.log('a user connected');
+ 
+  console.log('a user connected');
 
-      io.emit('start', scramble,scramble2, pile)
+ if (    socket.server.engine.clientsCount == 1) {
+
+      io.emit('start', scramble, scramble2, pileStart, true )
+
+  }
+  else{
+    io.emit('start', scramble, scramble2, pileStart, false)
+  }
+ 
+  
 
 
 
-    
+socket.on('play' ,(scram, id, player) => {
+
+  validation = false;
+
+  if (player == turn) {
 
 
-  });
+    if (verificationCard(scram) == true) {
+      if (player == 1) {
+        turn = 2
+      }
+      if (player == 2) {
+        turn = 1
+      }
+      lastColor = cards[scram].color;
+      lastNumber = cards[scram].number;
+
+      for (let i = 0; i < game.length; i++) {
+        if (game[i] == null) {
+          game[i] = scram;
+        }
+
+        if (cards[scram].number == "()") {
+          chooseColor("#0f0f0f", true, scram);
+        }
+
+        if (cards[scram].number == "M4") {
+          if (player == 1) {
+            alert("+4 cartas para p2");
+            addFourCards(1, id);
+          }
+          if (player == 2) {
+            alert("+4 cartas para p1");
+            addFourCards(2, id);
+          }
+        }
+
+        if (cards[scram].number == "M2") {
+          if (player == 1) {
+            alert("+2 cartas para p2");
+            addTwoCards(1, id);
+          }
+          if (player == 2) {
+            alert("+2 cartas para p1");
+            addTwoCards(2, id);
+          }
+        }
+
+        pile = `
+          <div  style="background-color:${cards[scram].color
+          };" class="card" id="${i}" > 
+          <div class="cornerUpDiv">
+          <span id="cornerUp">${specialCard(
+            cards[scram].number,
+            "u",player
+          )}</span> </div> <span>${specialCard(
+            cards[scram].number,
+            "m",player
+          )}</span> 
+          <div class="cornerDownDiv">
+          <span id="cornerDown">${specialCard(
+            cards[scram].number,
+            "d",player
+          )}</span></div> 
+          </div> `;
+
+        if (player == 1) {
+          scramble.splice(id, 1);
+        }
+        if (player == 2) {
+          scramble2.splice(id, 1);
+        }
+
+        break;
+      }
+    }
+
+  }
+
+
+
+  console.log("turno: " + turn)
+  io.emit('showCard', scramble, scramble2, pile)
+
+} )
+
+
+});
